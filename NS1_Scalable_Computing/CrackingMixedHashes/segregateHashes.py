40453df4645372c5866fef65c3181d1d3e69bf47
Arun Thundyill Saseendran <ats0stv@gmail.com>
2018-10-02 21:12:02 +0100

    Added the script to segregate hashes based on the hash type

---
+++ NS1_Scalable_Computing/CrackingMixedHashes/segregateHashes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""	Script to Segregate the Hash Types
+	Scalable Computing 
+"""
+__author__ = "Arun Thundyill Saseendran"
+__version__ = "0.0.1"
+__maintainer__ = "Arun Thundyill Saseendran"
+__email__ = "thundyia@tcd.ie"
+
+
+filename = "./input/thundyia1.hashes"
+newFilePrefix = "./segregatedHashes/thundyiaNew"
+
+formatDict = {"wierdhash":[],"noDollarHash":[]}
+with open(filename, "r") as inputFile:
+	for line in inputFile:
+		if '$' in line:
+			splittedLine = line.split('$')
+			# print splittedLine
+			if len(splittedLine) > 1:
+				if splittedLine[1] in formatDict:
+					formatDict[splittedLine[1]].append(line)
+				else:
+					formatDict[splittedLine[1]] = [line]
+			else:
+				formatDict["wierdHash"].append(line)
+		else:
+			formatDict["noDollarHash"].append(line)
+
+for key,value in formatDict.items():
+	print key +"  "+ str(len(value))
+	with open(newFilePrefix+"-"+key+".hashes","w") as outputFile:
+		for item in value:
+			outputFile.write(item)
