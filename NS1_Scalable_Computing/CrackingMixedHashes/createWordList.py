d1ec588d4a70f05268a81b5ff78aa55dd0f58f2a
Arun Thundyill Saseendran <ats0stv@gmail.com>
2018-10-23 02:16:51 +0100

    Added convert modes for selecting words that contain a specific set of characters

--- NS1_Scalable_Computing/CrackingMixedHashes/createWordList.py
+++ NS1_Scalable_Computing/CrackingMixedHashes/createWordList.py
@@ -16,6 +16,7 @@ LETTER_COUNT = 0
 MODE = -1
 MASTERFILE = './master.file'
 TOTAL_PROCESSED = 0
+REGEX_LOWER = '^[a-z]+$'
 
 def processArgs():
     global LETTER_COUNT
@@ -27,7 +28,7 @@ def processArgs():
     parser.add_argument("--outputFile", "-o", help="The path of the output file. If not specified file called (Should not be a hidden file) "
        +OUTPUT_FILE+ "in CWD will be created", required=False)
     parser.add_argument("--numberOfChars", "-n", help="The number of letters. Eg. 4", required=True)
-    parser.add_argument("--convert", "-c", help="1 - Lower; 2 - Upper; 3 - As is", required=True)
+    parser.add_argument("--convert", "-c", help="1 - Lower; 2 - Upper; 3 - As is, 4 - Select lower, 5 - select alnum", required=True)
     args = parser.parse_args()
     MASTERFILE = args.masterFile
     if args.outputFile:
@@ -39,8 +40,8 @@ def processArgs():
         exit(1)
     try:
         MODE = int(args.convert)
-        if not (MODE > 0 and MODE <= 3):
-            print 'Mode should be within 1 and 3'
+        if not (MODE > 0 and MODE <= 5):
+            print 'Mode should be within 1 and 5'
             exit(2)
     except Exception as e:
         print 'Unable to parse the mode'
@@ -64,9 +65,34 @@ def convertBasedOnMode(text, mode):
         return text.upper()
     elif mode == 1:
         return text.lower()
+    elif mode ==4 or mode == 5 :
+        return text
     else:
         print 'Unacceptable text conversion mode'
         exit(2)
+
+def isLetterInWord(line):
+    chars = ['a','e','o','i','h','s','n','l','r','u','t','c','m']
+    chars = ['a','e','o','i','h']
+    chars1 = ['e','o','i','h']
+    chars2 = ['s','n','l','r','u','t','c','m','d','p','b','g','y']
+    # for char in chars:
+    #     if char in line:
+    # cvvcv
+    # vvcvv
+    # vvcvc
+
+    data = line.strip()
+    counter = 0
+    if data[0] in chars2 and data[1] in chars and data[2] in chars and data[3] in chars2 and data[4] in chars:
+        return True
+    elif data[0] in chars1 and data[1] in chars1 and data[2] in chars2 and data[3] in chars and data[4] in chars:
+        return True
+    elif data[0] in chars1 and data[1] in chars1 and data[2] in chars2 and data[3] in chars and data[4] in chars2:
+        return True
+    else:
+        return False
+
 
 def processFile(masterFile, outputFile, convertMode, letterCount):
     global TOTAL_PROCESSED
@@ -74,9 +100,19 @@ def processFile(masterFile, outputFile, convertMode, letterCount):
         for line in file:
             # print 'Reading line {}'.format(line)
             if len(line.strip()) == letterCount:
-                TOTAL_PROCESSED = TOTAL_PROCESSED + 1
+                if MODE == 4:
+                    if re.match(REGEX_LOWER,line) and isLetterInWord(line):
+                        TOTAL_PROCESSED = TOTAL_PROCESSED + 1
+                        appendToFile(outputFile, convertBasedOnMode(line.strip(),convertMode))
+                elif MODE == 5:
+                    if unicode(line.strip()).isalnum():
+                        TOTAL_PROCESSED = TOTAL_PROCESSED + 1
+                        appendToFile(outputFile, convertBasedOnMode(line.strip(),convertMode))
+                else:
+                    appendToFile(outputFile, convertBasedOnMode(line.strip(),convertMode))
+                    TOTAL_PROCESSED = TOTAL_PROCESSED + 1
                 # print ' **** **** *** Word match for word {}'.format(line.strip())
-                appendToFile(outputFile, convertBasedOnMode(line.strip(),convertMode))
+                
 
 def main():
     processArgs()
