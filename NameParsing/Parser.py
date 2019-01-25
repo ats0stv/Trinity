b01731f611138c1bb1b563fec9586bada2d64bb4
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-01-25 00:38:50 +0000

    Wired the Argparser and updated the log format

--- NameParsing/Parser.py
+++ NameParsing/Parser.py
@@ -7,18 +7,30 @@ import os
 import logging
 import argparse
 
-from Commons.Constants import (LOG_FILENAME, DEBUG_LEVEL)
+from Commons.ArgsParser import ArgsParser
+from Commons.Constants import (LOG_FILENAME, DEBUG_LEVEL,
+                               LOG_FORMAT)
+
+ARGS = None
 
 if not os.path.exists('./logs'):
     os.makedirs('./logs')
 
-logging.basicConfig(filename=LOG_FILENAME, level=DEBUG_LEVEL)
+logging.basicConfig(filename=LOG_FILENAME, level=DEBUG_LEVEL,
+                    format=LOG_FORMAT)
 logger = logging.getLogger('Parser')
 
 def main():
-    logging.info('Starting the parsing of names')
-    logging.debug('Parsing arguments')
-    
+    global ARGS
+    try:
+        logger.info('Starting the parsing of names')
+        logger.debug('Parsing arguments')
+        argParser = ArgsParser()
+        ARGS = argParser.parseArguments()
+        logger.debug(ARGS)
+    except Exception as e:
+        logger.error(f"Error in the start script. Error = {e}")
+        exit(1)
 
 if __name__ == "__main__":
     main()
