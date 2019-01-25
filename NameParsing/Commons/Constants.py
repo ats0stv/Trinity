5f000cfcb4dcd20a629acf5d4b64f5d7c45bfd56
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-01-25 01:49:58 +0000

    Added the constants required for name parsing

--- NameParsing/Commons/Constants.py
+++ NameParsing/Commons/Constants.py
@@ -1,5 +1,24 @@
 import logging
 
+# Logging configuration
 LOG_FILENAME = 'logs/Parser.log'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 DEBUG_LEVEL = logging.DEBUG
+
+# JSON/XML element names
+TITLE = "title"
+FORENAME = "forename"
+SURNAME = "surname"
+
+# Default Title if title could not be detemined
+DEFAULT_TITLE = ""
+
+
+# Lowercase list of possible title representations
+TITLE_KEY_DICT = {
+    "Mr.": ["male", "m", "man", "guy"],
+    "Ms.": ["female", "f", "woman", "girl"],
+    "Dr.": ["doctor", "doc"],
+    "Prof.": ["professor", "prof"],
+    "Sir.": ['british knight', 'dame', 'sir']
+}
