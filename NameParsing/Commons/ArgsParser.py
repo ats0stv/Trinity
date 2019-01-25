4a6505fa02733fea0b3049916f0fa42570809962
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-01-25 03:22:44 +0000

    Updated the help text in argparse

--- NameParsing/Commons/ArgsParser.py
+++ NameParsing/Commons/ArgsParser.py
@@ -19,8 +19,8 @@ class ArgsParser:
         parser.add_argument("--outputFile", "-o", help="If a file path is present, it writes the output to the file, else on console", required=False)
         parser.add_argument('--pretty', '-p', help="Pretty print the JSON/XML output", action='store_true', required=False)
         outputTypeGroup = parser.add_mutually_exclusive_group(required=True)
-        outputTypeGroup.add_argument('--xml', action='store_true')
-        outputTypeGroup.add_argument('--json', action='store_true')
+        outputTypeGroup.add_argument('--xml', help="Render output as XML", action='store_true')
+        outputTypeGroup.add_argument('--json', help="Render output as JSON", action='store_true')
         args = parser.parse_args()
         
         if self._validateArgs(args) and self._createOutputDirIdenpotent(args.outputFile):
