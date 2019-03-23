a6a63b8e1eafe32295438b1fbf6e545d5e64fc87
Arun Thundyill Saseendran <ats0stv@gmail.com>
2019-03-23 15:41:51 +0000

    Delete Sensor Data Processing Files

--- SensorDataProcessing/Configuration/AppConfig.py
+++
@@ -1,9 +0,0 @@
-# Log Vars
-LOG_DIR = './logs'
-
-# Phone Sensor URL
-PHONE_URL = 'http://172.17.0.121:9000/gathered/latest'
-
-# FILE IO
-JSON_DUMP_FILE = 'Outputs/sensorData.json'
-OUTPUT_CSV = 'Outputs/sensorData.csv'
